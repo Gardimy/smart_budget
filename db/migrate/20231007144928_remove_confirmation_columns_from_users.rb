75da2b9ff5a64f679092894bf345b1a65f743962
Gardimy Charles <gardimycharles889@gmail.com>
2023-10-10 19:42:45 -0400

    fix bug

--- db/migrate/20231007144928_remove_confirmation_columns_from_users.rb
+++ db/migrate/20231007144928_remove_confirmation_columns_from_users.rb
@@ -1,7 +1,5 @@
 class RemoveConfirmationColumnsFromUsers < ActiveRecord::Migration[7.0]
   def change
-    remove_column :users, :confirmation_token, :string
-    remove_column :users, :confirmed_at, :datetime
-    remove_column :users, :confirmation_sent_at, :datetime
+  remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at
   end
 end
